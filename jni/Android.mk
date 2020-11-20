ee75a07ba880b6470115846cc06772bf3b8d36c6
Bernardo Ramos <berna.gensis@gmail.com>
2020-11-20 00:46:04 -0300

    update to binn v3

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,8 +1,8 @@
-LOCAL_PATH := $(call my-dir)/binn
+LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)  
 
-LOCAL_SRC_FILES := src/binn.c
+LOCAL_SRC_FILES := ../../binn/src/binn.c
 
 LOCAL_MODULE := libbinn
 
